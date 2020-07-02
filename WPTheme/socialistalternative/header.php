b9ca973bd31883bc24ef25bba71b703536203865
Sofia Wiking <aswiking@gmail.com>
2020-07-02 14:33:08 +0100

    changed font again... I think this one will do for now

--- WPTheme/socialistalternative/header.php
+++ WPTheme/socialistalternative/header.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=0.8"/>
         <title>Socialist Alternative</title>
         <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/main.css">
-        <link href="https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&family=Work+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,400;1,700;1,800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Candal&family=Work+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,400;1,700;1,800&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
     <body>
