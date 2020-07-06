481ab2eab5856e011af4a510e320fc6381a09d91
FJRay <1960sbatman@gmail.com>
2020-07-06 21:51:25 +0100

    Merged no margin into events and added stuff to get text from wordpress

--- WPTheme/socialistalternative/page-events.php
+++ WPTheme/socialistalternative/page-events.php
@@ -4,14 +4,21 @@
 	Template Post Type: post, page, event
 	*/
     get_header();
-    echo '<main class="salt-maincontent">';
+    echo '<main class="salt-maincontent salt-nomargin">';
+    
     ?>
     <div class="salt-infoblock">
-		<h2>FOR A SOCIALIST ALTERNATIVE TO AUSTERITY, CLIMATE CHANGE AND ALL OPPRESSION</h2>
-		As part of an international with presence in over 30 countries on all continents, we fight to advance a working class alternative across national borders, for a socialist world.
-		</div>
-		<div class="salt-homeblock salt-upcomingevents">
-		<h2>UPCOMING EVENTS</h2>
+		<?php
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+					get_template_part("content", "page");
+				}
+			}
+		?>
+	</div>
+	<div class="salt-homeblock salt-upcomingevents">
+	<h2>UPCOMING EVENTS</h2>
 		<ul>
 			<?php
 				$posts = get_posts(array('numberposts' => 3, 'category_name' => get_theme_mod('category_name',"events")));    
@@ -22,12 +29,13 @@
 				}
 			?>
 		</ul>
-		<a class="salt-moreevents" href="nowhere.html">Find events in my area</a>
-
-		</div>
-		<div class="salt-homeblock salt-international">
-			<h2>INTERNATIONAL</h2>
-		</div>
+		<a class="salt-moreevents" href="http://freds.website/salt/branch-directory/">Find more events local to you on our branches' Facebook pages</a>
+	</div>
+	<div class="salt-homeblock salt-international">
+		<h2>INTERNATIONAL</h2>
+		<p>We are proud to be part of a vibrant international organisation of dedicated socialist fighters, which has sections in 30 countries across the globe:<a href="http://internationalsocialist.net/" target="_blank" rel="noopener noreferrer" data-content="http://internationalsocialist.net" data-type="external">&nbsp;International Socialist Alternative</a>.&nbsp;</p>
+		<p><a href="https://internationalsocialist.net/"><img class="alignnone size-medium wp-image-262" src="http://freds.website/salt/wp-content/uploads/2020/06/isa-logo-300x123.png" alt="" width="300" height="123"></a></p>
+	</div>
     <?php
     echo '</main>';
     get_sidebar();
