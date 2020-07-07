bc87d1aa508267aa38f6dd24a754591e6ed6b102
Sofia Wiking <aswiking@gmail.com>
2020-07-07 15:01:40 +0100

    partway through resporing stylings for larger viewports

--- WPTheme/socialistalternative/header-fullwidth.php
+++ WPTheme/socialistalternative/header-fullwidth.php
@@ -18,8 +18,20 @@
 					<input name="search" placeholder="Search...">
 				</form>
             </h1>
-            <nav>
+
+                        
+            <input class="menu-btn" type="checkbox" id="menu-btn" /> 
+            <label class="menu-icon" for="menu-btn">
+            <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/hamburger.png" alt="Menu button">
+            </label>
+
+            <form class="salt-mainsearch" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
+				<input name="search" placeholder="Search...">
+			</form>
+            <nav class='menu'>
+                <label for="menu-btn" class='menu-close-btn'> 
+                    <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/cross-yellow.svg" alt="Close menu icon" > 
+                </label>
                 <?php wp_nav_menu(array('theme_location' => 'main-navbar')); ?>
-            </nav>
         </header>
         <div class="salt-bodycontainer">
