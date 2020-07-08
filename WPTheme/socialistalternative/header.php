9e8e50f7fcd12d075d46ab95994beb9a1ca7363d
Sofia Wiking <aswiking@gmail.com>
2020-07-08 15:14:12 +0100

    moved nav out of header and sorted small viewports accordingly

--- WPTheme/socialistalternative/header.php
+++ WPTheme/socialistalternative/header.php
@@ -16,20 +16,21 @@
                     <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/SA-logo-full-yellow.svg" alt="Socialist Alternative">
                 </a>    
             </h1>
-            
-            <input class="menu-btn" type="checkbox" id="menu-btn" /> 
-            <label class="menu-icon" for="menu-btn">
-            <i class="fas fa-bars"></i>
-            </label>
 
             <form class="salt-mainsearch" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
 				<input name="search" placeholder="Search...">
 			</form>
+        </header>
+        <div class="salt-mainmenu">
+            <input class="menu-btn" type="checkbox" id="menu-btn" /> 
+            <label class="menu-icon" for="menu-btn">
+                <i class="fas fa-bars"></i>
+            </label>
             <nav class='menu'>
                 <label for="menu-btn" class='menu-close-btn'> 
-                <i class="fas fa-times"></i>
+                    <i class="fas fa-times"></i>
                 </label>
                 <?php wp_nav_menu(array('theme_location' => 'main-navbar')); ?>
             </nav>
-        </header>
+        </div>
         <div class="salt-bodycontainer">
