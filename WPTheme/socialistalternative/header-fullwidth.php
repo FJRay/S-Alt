6362b9536f06685c4c854501281187b23e6b3759
Sofia Wiking <aswiking@gmail.com>
2020-07-09 12:06:32 +0100

    sorted fullwidth header to be responsive too

--- WPTheme/socialistalternative/header-fullwidth.php
+++ WPTheme/socialistalternative/header-fullwidth.php
@@ -6,32 +6,34 @@
         <title>Socialist Alternative</title>
         <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/main.css">
         <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,700,800,900&display=swap" rel="stylesheet">
+        <script defer src="<?php echo get_bloginfo('template_directory'); ?>/resources/fontawesome/js/all.js"></script>
         <?php wp_head(); ?>
     </head>
     <body class="salt-altcolors salt-fullwidth">
         <header class="salt-mainheader">
             <h1>
                 <a href="<?php echo get_bloginfo("wpurl"); ?>">
-                    <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/mainlogo.png" alt="Socialist Alternative">
-                </a>
-                <form class="salt-mainsearch" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
-					<input name="search" placeholder="Search...">
-				</form>
+                    <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/SA-logo-full-yellow.svg" alt="Socialist Alternative">
+                </a>    
             </h1>
 
-                        
-            <input class="menu-btn" type="checkbox" id="menu-btn" /> 
-            <label class="menu-icon" for="menu-btn">
-            <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/hamburger.png" alt="Menu button">
-            </label>
-
-            <form class="salt-mainsearch" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
+            <form class="salt-mainsearch searchbar" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
 				<input name="search" placeholder="Search...">
 			</form>
+        </header>
+        <div class="salt-mainmenu">
+            <input class="menu-btn" type="checkbox" id="menu-btn" /> 
+            <label class="menu-icon" for="menu-btn">
+                <i class="fas fa-bars"></i>
+            </label>
             <nav class='menu'>
                 <label for="menu-btn" class='menu-close-btn'> 
-                    <img src="<?php echo get_bloginfo('template_directory'); ?>/resources/cross-yellow.svg" alt="Close menu icon" > 
+                    <i class="fas fa-times"></i>
                 </label>
                 <?php wp_nav_menu(array('theme_location' => 'main-navbar')); ?>
-        </header>
+                <form class="salt-navsearch searchbar" action="<?php echo get_bloginfo("wpurl"); ?>" method="GET">
+				<input name="search" placeholder="Search...">
+			</form>
+            </nav>
+        </div>
         <div class="salt-bodycontainer">
