3ac1f78bf8a9504ec06694744aab74f3cc952200
Sofia Wiking <aswiking@gmail.com>
2020-07-14 11:21:09 +0100

    added components to content-page so they can be styled different to posts

--- WPTheme/socialistalternative/content-page.php
+++ WPTheme/socialistalternative/content-page.php
@@ -1,1 +1,20 @@
-<?php the_content(); ?>
+<article class="salt-defaultpage">
+    <div class="salt-pageheader">    
+        <?php
+            if (has_post_thumbnail()) {
+                echo get_the_post_thumbnail();
+            } else {
+                echo "<img src='".get_bloginfo('template_directory')."/resources/headerimage.jpg'> ";
+            }
+        ?>
+        <h1>
+            <?php the_title() ?>
+        </h1>
+    </div>
+    <div class="salt-pagemain">
+
+        <div class="salt-pagebody">
+            <?php the_content() ?>
+        </div>
+    </div>
+</article>
