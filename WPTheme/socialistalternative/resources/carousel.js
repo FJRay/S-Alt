c8cc2c2b32a472516ce3595cbff53d72c8340073
Sofia Wiking <aswiking@gmail.com>
2020-07-15 16:29:53 +0100

    did an actual carousel rather than just scrolling

---
+++ WPTheme/socialistalternative/resources/carousel.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("salt-articlepreview");
+  if (n > slides.length) {
+    slideIndex = 1; //makes you go back to the start when you click next on the last slide?
+  }
+  if (n < 1) {
+    slideIndex = slides.length;//makes you go to last slide when you click previous on first?
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
--- WPTheme/socialistalternative/sidebar.php
+++ WPTheme/socialistalternative/sidebar.php
@@ -1,4 +1,6 @@
 <div class="salt-sidebar">
+    <h2>NEWS</h2>
+    <div class="sidebar-content">
     <?php 
         $sidebar_loop = new WP_Query(array(
             "category_name" => "Uncategorized",
@@ -11,4 +13,12 @@
             }
         }
     ?>
+    </div>
+    <div class='sidebar-buttons'>
+        <i class="fas fa-chevron-left prev" onclick="plusSlides(-1)"></i>
+        <i class="fas fa-chevron-right next" onclick="plusSlides(1) "></i>
+
+    </div>
+
+
 </div>
