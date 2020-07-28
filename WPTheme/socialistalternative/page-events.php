cdbb51b889e99197060d349c597c5636a7f3060b
Sofia Wiking <aswiking@gmail.com>
2020-07-28 10:43:47 +0100

    added some classes and a border around default image for small vps

--- WPTheme/socialistalternative/page-events.php
+++ WPTheme/socialistalternative/page-events.php
@@ -19,9 +19,21 @@
 		?>
 	</div>
 	<div class="salt-homeblock salt-upcomingevents">
+	<h2>UPCOMING EVENTS</h2>
+		<ul>
+			<?php
+				$posts = get_posts(array('numberposts' => 3, 'category_name' => get_theme_mod('category_name',"events")));    
+				foreach ($posts as $post) {
+					setup_postdata($post); 
+					the_post();
+					get_template_part("content", "event");
+				}
+			?>
+		</ul>
+		<a class="salt-moreevents" href="http://freds.website/salt/branch-directory/">Find more events local to you on our branches' Facebook pages</a>
+	</div>
 	
-	</div>
-	</div>
+	
     <?php
     echo '</main>';
     get_sidebar();
